bcc1d7157a0338418f4741daff4c75be66dcf166
LPapakostas <lpapakostas95@yahoo.com>
2023-02-16 12:47:52 +0200

    Change logic behind GA optimization as well as fitness function

--- GA_Optimization.py
+++ GA_Optimization.py
@@ -1,10 +1,9 @@
 import torch
-import pygad.torchga
 import pygad
-import pandas as pd
 import os
 import pickle
 import yaml
+import numpy as np
 
 # TODO: Add docstrings
 
@@ -13,6 +12,14 @@ WEIGHTS_PATH = "/nn_weights/MaximumTemperatureNN_Model_Parameters.pt"
 PREPROCESS_VALUES_PATH = "/nn_weights/MaximumTemperatureNN_training_preprocess_values.pickle"
 DATA_PATH = "/data/nn1/"
 GA_PARAMETERS_PATH = "/data/ga/parameters.yaml"
+
+K = 750
+A = 1400
+PLATE_THICKNESS = 0.005
+Y_COORDINATE = 0.02 + 0.004
+Z_COORDINATE = 0.0
+X_COORDINATES = [0.0, 0.015, 0.035, 0.05]
+EPS = 0.000001
 
 # *==== Class Declaration ====*
 
@@ -46,29 +53,57 @@ class MaxTemperatureNN(torch.nn.Module):
 
 # *==== Method Definitions ====*
 
-# TODO: Define (again) fitness function
+def normalize(input, mean, std):
+    return (input - mean) / std
+
+
 def fitness_func(solution, sol_idx):
     """
+    Genetic algorithm fitness function
     """
-    global data_inputs, data_outputs, torch_ga, model, loss_function
+    global model, mean, std
 
-    model_weights_dict = pygad.torchga.model_weights_as_dict(model=model,
-                                                             weights_vector=solution)
+    assert (len(solution) == 3)
+    initial_temperature = solution[0]
+    heat_input = solution[1]
+    electrode_velocity = solution[2]
 
-    # Use the current solution as the model parameters.
-    model.load_state_dict(model_weights_dict)
+    F, R1, R2 = [], [], []
+    for x in X_COORDINATES:
+        # NN model prediction
+        input = np.array([[PLATE_THICKNESS, initial_temperature, heat_input,
+                           electrode_velocity, x, Y_COORDINATE, Z_COORDINATE]])
+        input_scaled = normalize(input, mean, std).astype(np.float32)
+        max_temperature_prediction = model.predict(input_scaled)
 
-    predictions = model(data_inputs)
-    abs_error = loss_function(
-        predictions, data_outputs).detach().numpy() + 0.00000001
+        f_val = max_temperature_prediction - (K+A)/2
+        F.append(f_val)
 
-    solution_fitness = 1.0 / abs_error
+        # Constrain check for each prediction
+        r1_val, r2_val = 0, 0
 
+        if (max_temperature_prediction < K):
+            r1_val = 1
+        R1.append(r1_val)
+
+        if (max_temperature_prediction > A):
+            r2_val = 1
+        R2.append(r2_val)
+
+    # Calculate total cost
+    cost_values = np.power(np.array(F) - (K+A)/2, 2) + \
+        np.array(R1)*np.power(np.array(F) - K, 2) + \
+        np.array(R2)*np.power(np.array(F) - A, 2)
+    total_cost = np.sum(cost_values)
+
+    # Fitness function of GA
+    solution_fitness = 1.0 / (total_cost + EPS)
     return solution_fitness
 
 
 def generation_callback(ga_instance):
     """
+    Callback for optimization info
     """
     print("Generation = {generation}".format(
         generation=ga_instance.generations_completed))
@@ -78,58 +113,55 @@ def generation_callback(ga_instance):
     return
 
 
-def normalize(input, mean, std):
-    return (input - mean) / std
-
-
-def get_data():
+def load_values():
     """
+    Helper method that reads cached values about neural network definition
     """
-
     home_folder = os.getcwd()
+    model = MaxTemperatureNN(input=7, output=1)
+    model.load_state_dict(torch.load(
+        home_folder+WEIGHTS_PATH, map_location=torch.device("cpu")))
+    model.to("cpu")
 
-    # Read and save preporcessing values
-    param_filename = home_folder + PREPROCESS_VALUES_PATH
-    with open(param_filename, 'rb') as handle:
-        train_values_data = pickle.load(handle)
+    # Read and save preprocessing values
+    preprocess_filename = home_folder + PREPROCESS_VALUES_PATH
+    with open(preprocess_filename, "rb") as pre_fname:
+        train_values_data = pickle.load(pre_fname)
     mean = train_values_data["mean"]
     std = train_values_data["std"]
 
-    train_data_filepath = home_folder + DATA_PATH
-    train_df = pd.read_csv(train_data_filepath + "train_dataset.csv")
-    X_train, Y_train = train_df.iloc[:, 0:-1], train_df.iloc[:, -1]
-
-    data_input_numpy = normalize(X_train.to_numpy(), mean, std)
-    data_input = torch.from_numpy(data_input_numpy).to(torch.float32)
-    data_output = torch.from_numpy(Y_train.to_numpy()).to(torch.float32)
-
-    return data_input, data_output
+    return model, mean, std
 
 
 if (__name__ == "__main__"):
 
-    model = MaxTemperatureNN(7, 1)
-    loss_function = torch.nn.functional.mse_loss
-    data_inputs, data_outputs = get_data()
+    # Obtain trained neural network
+    model, mean, std = load_values()
 
     # Parse GA optimization parameters
     ga_param_filename = os.getcwd() + GA_PARAMETERS_PATH
     with open(ga_param_filename) as param_file:
         ga_params = yaml.load(param_file, Loader=yaml.FullLoader)
 
-    # Run genetic algorithm optimization
-    torch_ga = pygad.torchga.TorchGA(model=model,
-                                     num_solutions=ga_params["number_of_solutions"])
+    # Create Genetic algorithm
+    gene_type = [int, int, float]
+    gene_space = [{'low': 0, 'high': 500},  # `InitialTemperature` parameter
+                  {'low': 100, 'high': 2000},  # For the P parameter
+                  {'low': 0.0001, 'high': 1.0}]  # For the ElectrodeVelocity parameter
     ga_instance = pygad.GA(num_generations=ga_params["number_of_generations"],
                            num_parents_mating=ga_params["number_of_parents_mating"],
-                           initial_population=torch_ga.population_weights,
                            fitness_func=fitness_func,
+                           sol_per_pop=ga_params["solutions_per_population"],
+                           num_genes=ga_params["number_of_genes"],
+                           gene_type=gene_type,
+                           gene_space=gene_space,
                            parent_selection_type=ga_params["parent_selection_type"],
+                           keep_parents=ga_params["keep_parents"],
                            crossover_type=ga_params["crossover_type"],
                            mutation_type=ga_params["mutation_type"],
                            mutation_percent_genes=ga_params["mutation_percent_genes"],
-                           keep_parents=ga_params["keep_parents"],
-                           on_generation=generation_callback)
+                           callback_generation=generation_callback
+                           )
     ga_instance.run()
 
     # Returning the details of the best solution.
@@ -139,16 +171,6 @@ if (__name__ == "__main__"):
     print("Index of the best solution : {solution_idx}".format(
         solution_idx=solution_idx))
 
-    # Fetch the parameters of the best solution.
-    best_solution_weights = pygad.torchga.model_weights_as_dict(model=model,
-                                                                weights_vector=solution)
-    model.load_state_dict(best_solution_weights)
-    predictions = model(data_inputs)
-    print("Predictions : n", predictions.detach().numpy())
-
-    abs_error = loss_function(predictions, data_outputs)
-    print("Absolute Error : ", abs_error.detach().numpy())
-
     # Plot Genetic Algorithm results
-    ga_instance.plot_result(
+    ga_instance.plot_fitness(
         title="Iteration vs. Fitness", linewidth=4)
